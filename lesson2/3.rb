7551f9e3f041d14fbb8e583b91f5eda4fbcfb2b7
dd <dd@mb-s.ru>
2017-05-22 01:07:14 +0300

    lesson 2 task 3

---
+++ lesson2/3.rb
@@ -0,0 +1,17 @@
+#Заполнить массив числами фибоначчи до 100
+arr = []
+(0..100).each do |x|
+  if x == 0 
+    arr << 0
+  elsif x == 1
+    arr << 1
+  else
+    arr << arr[-2] + arr[-1]
+  end
+end
+
+p arr
+
+for x in (0..100) do
+  #p x
+end
