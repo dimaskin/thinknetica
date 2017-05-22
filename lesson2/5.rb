69a7c202062383490272a57e1723474d041dc608
dimaskin <dimaskin-nv@yandex.ru>
2017-05-22 19:10:27 +0300

    lesson 2 task 5

--- lesson2/5.rb
+++ lesson2/5.rb
@@ -4,33 +4,35 @@
 #Год високосный, если он делится на четыре без остатка, но если он делится на 100 без остатка, это не високосный год. 
 #Однако, если он делится без остатка на 400, это високосный год. 
 #Таким образом, 2000 г. является особым високосным годом, который бывает лишь раз в 400 лет.
-#p "day?"
-#day = gets.chomp.to_i
-#p "month?"
-#month = gets.chomp.to_i
+require 'date'
+p "day?"
+day = gets.chomp.to_i
+p "month?"
+month = gets.chomp.to_i
 p "year?"
 year = gets.chomp.to_i
 
 def isYearLeap?(year)
   leapYear = false 
-  #p "1#{leapYear}" 
   if year%4 == 0 
     leapYear = true
-    #p "2#{leapYear}"
     if year%100 == 0
       leapYear = false
-      #p "3#{leapYear}"
       if year%400 == 0 
         leapYear = true
-        #p "4#{leapYear}"
       end
     end
   end
-  #p "5#{leapYear}"
   return leapYear
 end
 
 months = {1=>31, 2=>isYearLeap?(year) ? 29 : 28, 3=>31, 4=>30, 5=> 31, 6=>30, 7=>31, 8=>31, 9=>30, 10=>31, 11=>30, 12=>31}
-
-p isYearLeap?(year)
-p months
+i = 0
+months.each do |k,v| 
+  if k < month
+    i += v
+  end
+end
+i += day
+p "Количество дней с начала года #{i}"
+p "Проверка #{Date.new(year,month,day).yday}"
