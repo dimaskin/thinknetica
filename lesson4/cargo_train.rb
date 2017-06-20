ee409e8f38e4682b9595aa06eb9aea562a7c1dcf
dimaskin <dimaskin-nv@yandex.ru>
2017-06-20 18:58:03 +0300

    fix all except main.rb

--- lesson4/cargo_train.rb
+++ lesson4/cargo_train.rb
@@ -1,6 +1,5 @@
 class CargoTrain < Train
   def add_wagon(wagon)
-    #wagons << wagon if wagon.type == "cargo"
     wagons << wagon if wagon.class.to_s == "CargoWagon" && @speed == 0
   end
 end
--- lesson4/passenger_train.rb
+++ lesson4/passenger_train.rb
@@ -1,6 +1,5 @@
 class PassengerTrain < Train
   def add_wagon(wagon)
-  	#если метод super выполняет его сначала в родительском классе, то как добавить условие по типу вагона? переписать его совсем?
     wagons << wagon if wagon.class.to_s == "PassengerWagon" && @speed == 0
   end
 end
