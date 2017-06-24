c037f2770df0051fc10551aafa8d397ecf8cf962
dimaskin <dimaskin-nv@yandex.ru>
2017-06-24 19:46:11 +0300

    add InstanceCounter methods

--- lesson5/instance_counter.rb
+++ lesson5/instance_counter.rb
@@ -1,7 +1,12 @@
 module InstanceCounter
+  def self.included(base)
+    base.extend ClassMethods
+    base.send :include, InstanceMethods
+  end
+
   module ClassMethods
     def instances
-
+      
     end
   end
 
