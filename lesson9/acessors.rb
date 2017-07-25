cdf5fdca2fb7cb08dd76138a00340e27337e848e
dimaskin <dimaskin-nv@yandex.ru>
2017-07-25 12:42:52 +0300

    add ClassMethods attr_accessor_with_history to accessors.rb

--- lesson9/acessors.rb
+++ lesson9/acessors.rb
@@ -1,5 +1,20 @@
 module Acessors
-  def attr_accessor_with_history
+
+  def self.included(base)
+    base.extend ClassMethods 
+  end
+
+  module ClassMethods
+
+    def attr_accessor_with_history(*args)
+      args.each do |arg|
+        var_name = "@#{arg}".to_sym
+        define_method(arg) { instance_variable_get(var_name) }
+        define_method("#{arg}=".to_sym) do |value| 
+          instance_variable_set(var_name, value) 
+        end
+      end
+    end
 
   end
 end
