bce305fc56d9c9fecd80629072d4ee52e596a70c
dimaskin <dimaskin-nv@yandex.ru>
2017-07-25 16:35:26 +0300

    add i_v_g, i_v_s, arg_history to accessors.rb

--- lesson9/accessors.rb
+++ lesson9/accessors.rb
@@ -1,4 +1,4 @@
-module Acessors
+module Accessors
 
   def self.included(base)
     base.extend ClassMethods 
@@ -9,10 +9,14 @@ module Acessors
     def attr_accessor_with_history(*args)
       args.each do |arg|
         var_name = "@#{arg}".to_sym
-        define_method(arg) { instance_variable_get(var_name) }
+        define_method(arg) { instance_variable_get(var_name) } 
         define_method("#{arg}=".to_sym) do |value| 
-          instance_variable_set(var_name, value) 
+          instance_variable_set(var_name, value)
+          @store ||= {}
+          @store[var_name] ||= []
+          @store[var_name] << value
         end
+        define_method("#{arg}_history".to_sym) { @store[var_name] }
       end
     end
 
