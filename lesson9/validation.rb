41570ce110728ad1ed8dec409b4a95e27a1d5dee
dimaskin <dimaskin-nv@yandex.ru>
2017-07-25 20:00:43 +0300

    add 2 modules to validation.rb

--- lesson9/validation.rb
+++ lesson9/validation.rb
@@ -1,3 +1,15 @@
 module Validation
+  def self.included(base)
+    base.extend ClassMethods
+    base.send :include, InstanceMethods
+  end
+
+  module ClassMethods
+ 
+  end
+
+  module InstanceMethods
+
+  end
 
 end
