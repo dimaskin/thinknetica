dbafb383c7bcede8b8d57c26391bb10f0aeccb60
dimaskin <dimaskin-nv@yandex.ru>
2017-07-26 18:27:05 +0300

    add empty methods valid? && validate! to validation.rb

--- lesson9/validation.rb
+++ lesson9/validation.rb
@@ -5,10 +5,22 @@ module Validation
   end
 
   module ClassMethods
- 
+    #метод класса, 
+    class validate(attr_name, type_validate, *additional_args)
+
+    end
   end
 
   module InstanceMethods
+
+    def valid?
+      validate!
+    end
+
+    def validate!
+
+    end
+
 
   end
 
